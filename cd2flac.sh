4e0ab80bc9fa6a7766f90304ec9c5287499d8fc2
Rix <rixwoodling@gmail.com>
2024-09-21 16:31:52 -0700

    Update cd2flac.sh

--- cd2flac.sh
+++ cd2flac.sh
@@ -12,19 +12,12 @@ if ! command -v cdparanoia &> /dev/null; then echo "cdparanoia is not installed.
 if ! command -v flac &> /dev/null; then echo "flac is not installed."; exit 1; fi
 
 # Check if music.csv exists
-if [ ! -f "csv/music.csv" ]; then
-  echo "csv/music.csv file not found!"
-  exit 1
-fi
+if [ ! -f "csv/music.csv" ]; then echo "csv/music.csv file not found!"; exit 1; fi
 
 # Search for the CD identifier in csv/music.csv
 matches=$(grep -i "$1" csv/music.csv)
-
 # If no matches are found, exit with an error message
-if [ -z "$matches" ]; then
-  echo "No matches found for '$1' in csv/music.csv."
-  exit 1
-fi
+if [ -z "$matches" ]; then echo "No matches found for '$1' in csv/music.csv."; exit 1; fi
 
 # If multiple matches are found, display the list and ask the user to select
 if [ $(echo "$matches" | wc -l) -gt 1 ]; then
@@ -48,11 +41,14 @@ else
 fi
 
 # Rip CD to AIFF and convert to FLAC
-cdparanoia --output-aiff --abort-on-skip --batch --log-summary && \
-cdparanoia --verbose --search-for-drive --query 2>&1 | tee -a cdparanoia.log && \
-flac *.aiff --verify --best --delete-input-file 2>&1 | tee -a flac.log
+#cdparanoia --output-aiff --abort-on-skip --batch --log-summary && \
+#cdparanoia --verbose --search-for-drive --query 2>&1 | tee -a cdparanoia.log && \
+#flac *.aiff --verify --best --delete-input-file 2>&1 | tee -a flac.log
+
+echo "START RIPPING"
+echo "done"
 
 # Call the Python script with the selected CD identifier as an argument
-python3 meta2flac.py "$cd_identifier"
+#python3 meta2flac.py "$cd_identifier"
 
 
