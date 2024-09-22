e2d6dc354f8da99873b67f1d417ecc492e09ccaa
Rix <rixwoodling@gmail.com>
2024-09-21 18:12:30 -0700

    Update cd2flac.sh

--- cd2flac.sh
+++ cd2flac.sh
@@ -16,13 +16,16 @@ if [ ! -f "csv/music.csv" ]; then echo "csv/music.csv file not found!"; exit 1; 
 
 argument=$( grep -i "$1" csv/music.csv )
 
-echo "$argument"
 # Search for argument in csv, if no matches, exit
-#if [ -z $( grep -i "$1" csv/music.csv ) ]; then 
-#  echo "No matches found for '$1' in csv/music.csv."; exit 1
+if [ -z "$argument" ]; then 
+echo "no matches found for '$1' in csv/music.csv"; exit 1; fi
+
+if [ ! -z "$argument" ]; then
+matches=$( cat csv/music.csv | grep "$argument" | sed 's/, /__/g' | awk -F',' '{print $3" - "$5,"("$6")","["$13"]"}' | sed 's/\[\]//' | sed 's/__/, /g' | sed 's/\"//g' | uniq )
+echo "$matches"; exit 0; fi
 
 # If multiple matches are found, display the list and ask the user to select
-#elif [ $( echo $( grep -i "$1" csv/music.csv ) | wc -l ) -gt 1 ]; then
+#if [ "$( echo $ | wc -l ) -gt 1 ]; then
 #  echo "Multiple matches found for '$1':"
 #  matches=$( cat csv/music.csv | grep "$1" | sed 's/, /__/g' | awk -F',' '{print $3" - "$5,"("$6")","["$13"]"}' | sed 's/\[\]//' | sed 's/__/, /g' | sed 's/\"//g' | uniq )
 #  echo "$matches" | nl
@@ -48,8 +51,8 @@ echo "$argument"
 #cdparanoia --verbose --search-for-drive --query 2>&1 | tee -a cdparanoia.log && \
 #flac *.aiff --verify --best --delete-input-file 2>&1 | tee -a flac.log
 
-echo "START RIPPING"
-echo "done"
+#echo "START RIPPING"
+#echo "done"
 
 # Call the Python script with the selected CD identifier as an argument
 #python3 meta2flac.py "$cd_identifier"
