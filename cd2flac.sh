fd9893d8138e903f5d52d09f14d5acbda2f1cf60
Rix <rixwoodling@gmail.com>
2024-09-21 18:08:48 -0700

    Update cd2flac.sh

--- cd2flac.sh
+++ cd2flac.sh
@@ -14,31 +14,34 @@ if ! command -v flac &> /dev/null; then echo "flac is not installed."; exit 1; f
 # Check if music.csv exists
 if [ ! -f "csv/music.csv" ]; then echo "csv/music.csv file not found!"; exit 1; fi
 
+argument=$( grep -i "$1" csv/music.csv )
+
+echo "$argument"
 # Search for argument in csv, if no matches, exit
-if [ -z $( grep -i "$1" csv/music.csv ) ]; then 
-  echo "No matches found for '$1' in csv/music.csv."; exit 1
+#if [ -z $( grep -i "$1" csv/music.csv ) ]; then 
+#  echo "No matches found for '$1' in csv/music.csv."; exit 1
 
 # If multiple matches are found, display the list and ask the user to select
-elif [ $( echo $( grep -i "$1" csv/music.csv ) | wc -l ) -gt 1 ]; then
-  echo "Multiple matches found for '$1':"
-  matches=$( cat csv/music.csv | grep "$1" | sed 's/, /__/g' | awk -F',' '{print $3" - "$5,"("$6")","["$13"]"}' | sed 's/\[\]//' | sed 's/__/, /g' | sed 's/\"//g' | uniq )
-  echo "$matches" | nl
-  echo "Please select the number corresponding to the correct album:"
-  read -r selection
+#elif [ $( echo $( grep -i "$1" csv/music.csv ) | wc -l ) -gt 1 ]; then
+#  echo "Multiple matches found for '$1':"
+#  matches=$( cat csv/music.csv | grep "$1" | sed 's/, /__/g' | awk -F',' '{print $3" - "$5,"("$6")","["$13"]"}' | sed 's/\[\]//' | sed 's/__/, /g' | sed 's/\"//g' | uniq )
+#  echo "$matches" | nl
+#  echo "Please select the number corresponding to the correct album:"
+#  read -r selection
 
   # Validate the user's input
-  selected_line=$( echo "$matches" | sed -n "${selection}p" )
-  if [ -z "$selected_line" ]; then
-    echo "Invalid selection. Exiting."
-    exit 1
-  fi
-
+#  selected_line=$( echo "$matches" | sed -n "${selection}p" )
+#  if [ -z "$selected_line" ]; then
+#    echo "Invalid selection. Exiting."
+#    exit 1
+#  fi
+#fi
   # Extract the CD identifier from the selected line (assuming it's in the first column)
-  cd_identifier=$(echo "$selected_line" | cut -d',' -f1)
-else
-  # Only one match, so we use it directly
-  cd_identifier=$(echo "$matches" | cut -d',' -f1)
-fi
+#  cd_identifier=$(echo "$selected_line" | cut -d',' -f1)
+#else
+#  # Only one match, so we use it directly
+#  cd_identifier=$(echo "$matches" | cut -d',' -f1)
+#fi
 
 # Rip CD to AIFF and convert to FLAC
 #cdparanoia --output-aiff --abort-on-skip --batch --log-summary && \
