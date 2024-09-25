621eb6bb0f601669fea32af1795832003af3e927
Rix <rixwoodling@gmail.com>
2024-09-24 17:43:24 -0700

    Update cd2flac.sh

--- cd2flac.sh
+++ cd2flac.sh
@@ -132,7 +132,7 @@ echo "Renaming files..."
 cd "$PATH_FLAC" || exit 1
 count=1
 for flac_file in *.flac; do
-    track_name=$(echo "$TRACK_LIST" | sed -n "${count}p" | awk -F, '{print $4}' )
+    track_name=$(echo "$TRACK_LIST" | sed -n "${count}p" | awk -F, '{print $9}' )
     new_filename="${track_name}.flac"
     mv "$flac_file" "$new_filename"
     ((count++))
